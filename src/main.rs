0b2634e8a3a1733e6b3be93e7428b31caa9dcb46
leo <80543539+justleoo@users.noreply.github.com>
2022-03-18 21:26:29 -0300

    fix code

--- src/main.rs
+++ src/main.rs
@@ -27,25 +27,66 @@ fn main() {
     };
 
     // username and hostname
-    println!("               {white}{}{red}{}{white}{}", whoami::username(), "@", whoami::hostname(), red = colors::red, white = colors::white);
+    println!("               {white}{}{red}{}{white}{}",
+             whoami::username(),
+             "@", whoami::hostname(),
+             red = colors::red,
+             white = colors::white);
 
     // os
-    println!("{blue}   __   _      {cyan}{}{white}{}{cyan}{}", "os ", "  ~ ", whoami::distro(), blue = colors::blue, cyan = colors::cyan, white = colors::white);
+    println!("{blue}   __   _      {cyan}{}{white}{}{cyan}{}",
+             "os ",
+             "  ~ ",
+             whoami::distro(),
+             blue = colors::blue,
+             cyan = colors::cyan,
+             white = colors::white);
 
     // uptime
     match sys.uptime() {
-        Ok(uptime) => println!("{blue} _(  )_( )_    {yellow}upt {white} ~ {yellow}{:?}", uptime, white = colors::white, yellow = colors::yellow, blue = colors::blue),
-        Err(x) => println!("{yellow}uptime error {white}~ {yellow}{}", x, white = colors::white, yellow = colors::yellow)
+        Ok(uptime) => println!("{blue} _(  )_( )_    {yellow}upt {white} ~ {yellow}{:?}", uptime,
+        white = colors::white,
+        yellow = colors::yellow,
+        blue = colors::blue),
+        Err(x) => println!("{yellow}uptime error {white}~ {yellow}{}",
+                           x,
+                           white = colors::white,
+                           yellow = colors::yellow)
     }
 
     // wm
-    println!("{blue}(_   _    _)   {green}{}{white}{}{green}{}", "wm ", "  ~ ", wm, blue = colors::blue, green = colors::green, white = colors::white);
+    println!("{blue}(_   _    _)   {green}{}{white}{}{green}{}",
+    "wm ",
+    "  ~ ",
+    wm,
+    blue = colors::blue,
+    green = colors::green,
+    white = colors::white);
     
     // terminal
-    println!("{blue}  (_) (__)     {magenta}{}{white}{}{magenta}{}", "term", " ~ ", term, magenta = colors::magenta, white = colors::white, blue = colors::blue);
+    println!("{blue}  (_) (__)     {magenta}{}{white}{}{magenta}{}",
+    "term",
+    " ~ ",
+    term,
+    magenta = colors::magenta,
+    white = colors::white,
+    blue = colors::blue);
 
     // shell
-    println!("               {yellow_bright}{}{white}{}{yellow_bright}{}", "sh ", "  ~ ", shell, yellow_bright = colors::yellow_bright, white = colors::white);
+    println!("               {yellow_bright}{}{white}{}{yellow_bright}{}",
+             "sh ",
+             "  ~ ",
+             shell,
+             yellow_bright = colors::yellow_bright,
+             white = colors::white);
+
     // decoration
-    println!("               {red}{} {yellow}{} {cyan}{} {blue}{} {white}{}", "●", "●", "●", "●", "●", red = colors::red, blue = colors::blue, yellow = colors::yellow, cyan = colors::cyan, white = colors::white);
+    println!("               {red}{} {yellow}{} {cyan}{} {blue}{} {white}{}",
+             "●", "●", "●", "●", "●",
+             red = colors::red,
+             blue = colors::blue,
+             yellow = colors::yellow,
+             cyan = colors::cyan,
+             white = colors::white);
 }
+
