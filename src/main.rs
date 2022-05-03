c5c3bb1c9ed8e2f7f8046595c6c543a3b3135457
yxqsnz <yxqsnz@gmail.com>
2022-05-03 13:17:18 -0300

    improve code quality

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,47 @@
-extern crate systemstat;
 mod colors;
+mod utils;
+use colors::*;
 use std::env;
-use systemstat::{System, Platform};
-
+use systemstat::{Platform, System};
 fn main() {
     let sys = System::new();
 
     // get wm
-    let wm = env::var("DESKTOP_SESSION").unwrap_or_else(|_| {
-        env::var("XDG_CURRENT_DESKTOP").unwrap_or_else(|_| "!!UNKNOWN!!".to_owned())
-    });
+    let wm = utils::get_wm().unwrap_or_else(|| "unknown".to_string());
 
     // get terminal
-    let term = env::var("TERM").unwrap_or_else(|_| "!!UNKNOWN!!".to_owned());
+    let term = env::var("TERM").unwrap_or_else(|_| "unknown".to_owned());
 
     // get shell
-    let shell = match env::var("SHELL") {
-        Ok(var) => {
-            let split: Vec<_> = var.split('/').collect();
-            match split.last() {
-                Some(sh) => sh.to_string(),
-                    _ => "unknown".to_string(),
-            }
-        }
-         _ => "unknown".to_string(),
-    };
+    let shell = utils::get_shell().unwrap_or_else(|| "unknown".to_string());
 
     // username and hostname
-    println!("               {white}{}{red}{}{white}{}",
-             whoami::username(),
-             "@", whoami::hostname(),
-             red = colors::red,
-             white = colors::white);
+    println!(
+        "               {WHITE}{}{RED}@{RESET}{}",
+        whoami::username(),
+        whoami::hostname(),
+    );
 
     // os
-    println!("{blue}   __   _      {cyan}{}{white}{}{cyan}{}",
-             "os ",
-             "  ~ ",
-             whoami::distro(),
-             blue = colors::blue,
-             cyan = colors::cyan,
-             white = colors::white);
+    println!(
+        "{BLUE}   __   _      {CYAN}os {WHITE}  ~ {CYAN}{}",
+        whoami::distro(),
+    );
 
     // uptime
-    match sys.uptime() {
-        Ok(uptime) => println!("{blue} _(  )_( )_    {yellow}upt {white} ~ {yellow}{:?}", uptime,
-        white = colors::white,
-        yellow = colors::yellow,
-        blue = colors::blue),
-        Err(x) => println!("{yellow}uptime error {white}~ {yellow}{}",
-                           x,
-                           white = colors::white,
-                           yellow = colors::yellow)
+    if let Ok(uptime) = sys.uptime() {
+        println!("{BLUE} _(  )_( )_    {YELLOW}upt {WHITE} ~ {YELLOW}{uptime:?}",)
     }
 
     // wm
-    println!("{blue}(_   _    _)   {green}{}{white}{}{green}{}",
-    "wm ",
-    "  ~ ",
-    wm,
-    blue = colors::blue,
-    green = colors::green,
-    white = colors::white);
-    
+    println!("{BLUE}(_   _    _)   {GREEN}wm {WHITE}  ~ {GREEN}{wm}");
+
     // terminal
-    println!("{blue}  (_) (__)     {magenta}{}{white}{}{magenta}{}",
-    "term",
-    " ~ ",
-    term,
-    magenta = colors::magenta,
-    white = colors::white,
-    blue = colors::blue);
+    println!("{BLUE}  (_) (__)     {MAGENTA}term{WHITE} ~ {MAGENTA}{term}");
 
     // shell
-    println!("               {yellow_bright}{}{white}{}{yellow_bright}{}",
-             "sh ",
-             "  ~ ",
-             shell,
-             yellow_bright = colors::yellow_bright,
-             white = colors::white);
+    println!("               {YELLOW_BRIGHT}sh {WHITE}  ~ {YELLOW_BRIGHT}{shell}");
 
     // decoration
-    println!("               {red}{} {yellow}{} {cyan}{} {blue}{} {white}{}",
-             "●", "●", "●", "●", "●",
-             red = colors::red,
-             blue = colors::blue,
-             yellow = colors::yellow,
-             cyan = colors::cyan,
-             white = colors::white);
+    println!("               {RED}● {YELLOW}● {CYAN}● {BLUE}● {WHITE}●")
 }
-
